c8625fc4eb5c90f44155d72ff1b0be41586a63bb
Kodotautas <lukosiunas.v@gmail.com>
2022-12-04 21:41:02 +0200

    working on gui

--- 2_keyboard_calculator/src_go_version_gui/main.go
+++ 2_keyboard_calculator/src_go_version_gui/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
 )
 
 var task string
@@ -10,26 +10,30 @@ var task string
 /* ------------------------------ MAIN FUNCTION ----------------------------- */
 // main function to run the program
 func main() {
-	fmt.Println("Write 'start' or 'stop':")
-	fmt.Scanln(&task)
-	while := true
-	for while {
-		// start or stop program based on user input
-		if len(task) > 1 {
-			if task == "start" {
-				input_variables()
-				calculator()
-			} else if task == "stop" {
-				fmt.Println("Program exits.")
-				//exit program
-				os.Exit(0)
-			} else {
-				println("Invalid argument. Use 'start' or 'stop'")
-				main()
-			}
-		} else {
-			println("No argument or invalid argument. Use 'start' or 'stop'")
-			main()
-		}
-	}
+	a := app.New()
+	w := a.NewWindow("Hello World")
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
+	// fmt.Println("Write 'start' or 'stop':")
+	// fmt.Scanln(&task)
+	// while := true
+	// for while {
+	// 	// start or stop program based on user input
+	// 	if len(task) > 1 {
+	// 		if task == "start" {
+	// 			input_variables()
+	// 			calculator()
+	// 		} else if task == "stop" {
+	// 			fmt.Println("Program exits.")
+	// 			//exit program
+	// 			os.Exit(0)
+	// 		} else {
+	// 			println("Invalid argument. Use 'start' or 'stop'")
+	// 			main()
+	// 		}
+	// 	} else {
+	// 		println("No argument or invalid argument. Use 'start' or 'stop'")
+	// 		main()
+	// 	}
+	// }
 }
